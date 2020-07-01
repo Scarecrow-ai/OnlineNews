4da968e55ce036903a3eb2b5aea70281f8ac5b2f
V <2366532530@qq.com>
2020-07-01 17:37:10 +0800

    Default Changelist

--- src/com/news/Test.java
+++ src/com/news/Test.java
@@ -3,14 +3,16 @@ package com.news;
 import com.news.dao.NewsDao;
 import com.news.entity.News;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<News> result = NewsDao.selectNews("好");
-        for (News news : result) {
-            System.out.println(news.getNewsTitle());
-        }
+//        List<News> result = new ArrayList<>();
+//        result.add(NewsDao.selectNews(1));
+//        for (News news : result) {
+//            System.out.println(news.getNewsTitle());
+//        }
+        NewsDao.delete(50);
     }
 }
--- src/com/news/dao/NewsDao.java
+++ src/com/news/dao/NewsDao.java
@@ -1,7 +1,6 @@
 package com.news.dao;
 
 import com.news.entity.News;
-import com.news.utils.DButils;
 import com.news.utils.NewsDButils;
 
 import java.io.IOException;
@@ -14,15 +13,40 @@ public interface NewsDao {
     /**
      * 删除
      */
-    static boolean delete(News news) {
-        return false;
+    static boolean delete(int newsId) {
+        boolean deleteFinished = false;
+        String sql = "DELETE FROM news WHERE newsId=" + newsId;
+        NewsDButils dButils = new NewsDButils();
+        try {
+            dButils.getConnect();
+            dButils.updateTable(sql);
+            dButils.getClose();
+            deleteFinished = true;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return deleteFinished;
     }
 
     /**
      * 修改
      */
     static boolean update(News news) {
-        return false;
+        boolean updateFinished = false;
+        String sql = "UPDATE news SET newsTitle='" + news.getNewsTitle() +
+                "' , newsText='" + news.getNewsText()
+                + "', newsLabel='" + news.getNewsLabel() + "'"
+                + "WHERE newsId=" + news.getNewsId();
+        NewsDButils dButils = new NewsDButils();
+        try {
+            dButils.getConnect();
+            dButils.updateTable(sql);
+            dButils.getClose();
+            updateFinished = true;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return updateFinished;
     }
 
     /**
@@ -52,5 +76,29 @@ public interface NewsDao {
             e.printStackTrace();
         }
         return matchNews;
+    }
+
+    static News selectNews(int id) {
+        String sql = "SELECT newsID, newsTitle, newsText, newsCount,newsLabel FROM news " +
+                "WHERE newsID=" + id;
+        ResultSet rs;
+        News targetNews = null;
+        NewsDButils dButils = new NewsDButils();
+        try {
+            dButils.getConnect();
+            rs = dButils.selectTable(sql);
+            while (rs.next()) {
+                targetNews = new News(rs.getInt("newsId")
+                        , rs.getString("newsTitle")
+                        , rs.getString("newsText")
+                        , rs.getInt("newsCount")
+                        , rs.getString("newsLabel")
+                );
+            }
+            dButils.getClose();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return targetNews;
     }
 }
